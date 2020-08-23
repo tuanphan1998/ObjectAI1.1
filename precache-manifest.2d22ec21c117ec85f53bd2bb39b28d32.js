63bde71146fe26b0c9e56edcacfa2ceabe3c24b2
TUanphan nam <phantuanw.o.t@gmail.com>
2020-08-23 09:35:05 +0700

    Updates

---
+++ precache-manifest.2d22ec21c117ec85f53bd2bb39b28d32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a7dbe12971f268b50bf",
+    "url": "/ObjectAI1.1/static/css/main.4e35fe55.chunk.css"
+  },
+  {
+    "revision": "1a7dbe12971f268b50bf",
+    "url": "/ObjectAI1.1/static/js/main.2060fae9.chunk.js"
+  },
+  {
+    "revision": "9563df5a91109dec4152",
+    "url": "/ObjectAI1.1/static/js/runtime~main.898d0dee.js"
+  },
+  {
+    "revision": "3c40de53f52fff927154",
+    "url": "/ObjectAI1.1/static/js/2.ff2b755b.chunk.js"
+  },
+  {
+    "revision": "f25c34cfd895ac0579ded08522c57e5b",
+    "url": "/ObjectAI1.1/index.html"
+  }
+];
